2d018226532907e8b18e579f9980ae6563e7fc54
Dieimes Nunes <dieimes.nunes@docente.pr.senac.br>
2023-02-22 11:02:30 -0300

    Atualização de arquivos

---
+++ 02 - Aula 23-02-2023/conversao.py
@@ -0,0 +1,5 @@
+print(int("1") + 1)
+print(float("1") + 1)
+print(type(int("1") + 1))
+
+print (str(1))
---
+++ 02 - Aula 23-02-2023/variaveis.py
@@ -0,0 +1,7 @@
+nome = "Dieimes Nunes de Souza"
+idade = 2023 - 1990
+
+print (nome)
+print (str(idade)  + " anos")
+
+print (idade, "anos")
