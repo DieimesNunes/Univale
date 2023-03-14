cd374709be5426381662b57e259e748d0c4bf519
unknown <dieimes.nunes@docente.pr.senac.br>
2023-03-13 22:02:35 -0300

    incompleto

---
+++ 05 - Aula 13-03-2023/calculadora.py
@@ -0,0 +1,30 @@
+sair = "sair"
+while True:
+    numero1 = input ("Escreva o primeiro número: ")
+    numero2 = input ("Escreva o segundo número: ")
+    operador = input ("Digite o operador (+-*/): ")
+    numero1_float = 0
+    numero2_float = 0 
+
+    numero_valido= None   
+    try: # Tentar executar o código
+        numero1_float = float(numero1)
+        numero2_float = float(numero2)
+        numero_valido= True
+
+    except: # Caso tenha executar o código
+      numero_valido = False
+
+    if numero_valido == False:
+        print("Número inválido. ")
+        continue
+
+    operador_aceito= ("+-*/")
+    if operador not in operador_aceito:
+        print("Operador não aceito")
+    
+    sair = input ("Deseja sair? ").lower().startswith("s")
+    if sair is True:
+        break
+
+    
