9945c927a929d37aba0ddfd1397a9678478f7c37
unknown <dieimes.nunes@docente.pr.senac.br>
2023-03-13 21:01:31 -0300

    Aula 13/03

---
+++ 05 - Aula 13-03-2023/while2.py
@@ -0,0 +1,9 @@
+contador = 0
+while contador <= 100:
+    print(contador)
+    contador = contador +1
+
+    if contador == 24:
+        print ("Não vou exibir o número 24")        
+        print("Acabou")
+        continue
